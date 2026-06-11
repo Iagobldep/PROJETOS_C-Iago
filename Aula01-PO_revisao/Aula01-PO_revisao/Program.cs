dba0364863557bc9ddea4d789863a8cc40a647cf
Iagobldep <iagobldep@gmail.com>
2026-06-11 16:55:08 -0300

    Aula01-PO_revisao

---
+++ Aula01-PO_revisao/Aula01-PO_revisao/Program.cs
@@ -0,0 +1,16 @@
+﻿using Aula01_PO_revisao;
+using Aula01_PO_revisao.components;
+using System;
+using System.Numerics;
+using System.IO;
+using System.Diagnostics;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Universo universo = new Universo("Iagoneta", "Bluadep", 6093, new DateTime(2009, 10, 5));
+
+string mensagem = $"{universo.Planeta}\n{universo.Lua}\n{universo.MassaPlaneta}\n{universo.DataDescoberta}";
+
+Console.WriteLine(mensagem);
+
+universo.anotacoes(mensagem);
